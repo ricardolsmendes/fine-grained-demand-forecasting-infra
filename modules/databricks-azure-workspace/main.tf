cfe8e6d0f529571517a9915ef7c3b72670b82221
Ricardo Mendes <ricardolsmendes@gmail.com>
2025-02-28 17:16:01 -0300

    feat: set up the data lake layers (#3)

--- modules/databricks-azure-workspace/main.tf
+++ modules/databricks-azure-workspace/main.tf
@@ -55,8 +55,8 @@ resource "databricks_external_location" "dl_landing" {
   comment         = "Landing zone for the ${var.human_friendly_project_name} accelerator in the ${var.environment} environment."
 }
 
-resource "databricks_volume" "kaggle" {
-  name             = "kaggle"
+resource "databricks_volume" "dl_landing_blob_storage" {
+  name             = "blob_storage"
   catalog_name     = databricks_catalog.this.name
   schema_name      = databricks_schema.dl_landing.name
   volume_type      = "EXTERNAL"
@@ -89,4 +89,106 @@ resource "databricks_external_location" "dl_layers" {
   )
   credential_name = databricks_storage_credential.this.id
   comment         = "Data lake ${each.value.name} layer for the ${var.human_friendly_project_name} accelerator in the ${var.environment} environment."
+}
+
+resource "databricks_sql_table" "dl_bronze_kaggle_train" {
+  name               = "kaggle_train"
+  catalog_name       = databricks_catalog.this.name
+  schema_name        = databricks_schema.dl_layers["bronze"].name
+  table_type         = "EXTERNAL"
+  data_source_format = "DELTA"
+  storage_location   = "${databricks_external_location.dl_layers["bronze"].url}/kaggle/train"
+}
+
+resource "databricks_sql_table" "dl_silver_store_item_history" {
+  name               = "store_item_history"
+  catalog_name       = databricks_catalog.this.name
+  schema_name        = databricks_schema.dl_layers["silver"].name
+  table_type         = "EXTERNAL"
+  data_source_format = "DELTA"
+  storage_location   = "${databricks_external_location.dl_layers["silver"].url}/sales/store_item_history"
+}
+
+resource "databricks_sql_table" "dl_gold_store_item_forecasts" {
+  name               = "store_item_forecasts"
+  catalog_name       = databricks_catalog.this.name
+  schema_name        = databricks_schema.dl_layers["gold"].name
+  table_type         = "EXTERNAL"
+  data_source_format = "DELTA"
+  storage_location   = "${databricks_external_location.dl_layers["gold"].url}/sales/store_item_forecasts"
+
+  column {
+    name = "date"
+    type = "date"
+  }
+  column {
+    name = "store"
+    type = "int"
+  }
+  column {
+    name = "item"
+    type = "int"
+  }
+  column {
+    name = "sales"
+    type = "decimal(25, 18)"
+  }
+  column {
+    name = "sales_predicted"
+    type = "decimal(25, 18)"
+  }
+  column {
+    name = "sales_predicted_upper"
+    type = "decimal(25, 18)"
+  }
+  column {
+    name = "sales_predicted_lower"
+    type = "decimal(25, 18)"
+  }
+  column {
+    name = "training_date"
+    type = "date"
+  }
+
+  partitions = [
+    "date"
+  ]
+}
+
+resource "databricks_sql_table" "dl_gold_store_item_forecast_evals" {
+  name               = "store_item_forecast_evals"
+  catalog_name       = databricks_catalog.this.name
+  schema_name        = databricks_schema.dl_layers["gold"].name
+  table_type         = "EXTERNAL"
+  data_source_format = "DELTA"
+  storage_location   = "${databricks_external_location.dl_layers["gold"].url}/sales/store_item_forecast_evals"
+
+  column {
+    name = "store"
+    type = "int"
+  }
+  column {
+    name = "item"
+    type = "int"
+  }
+  column {
+    name = "mae"
+    type = "decimal(25, 18)"
+  }
+  column {
+    name = "mse"
+    type = "decimal(25, 18)"
+  }
+  column {
+    name = "rmse"
+    type = "decimal(25, 18)"
+  }
+  column {
+    name = "training_date"
+    type = "date"
+  }
+
+  partitions = [
+    "training_date"
+  ]
 }
